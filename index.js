e53df7bf610f909073daef3a107a93bd1cbcff05
Arsen Toma <58401435+arsen009@users.noreply.github.com>
2023-11-26 22:50:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+
+var imagesArray = [
+'images/dice1.png',
+'images/dice2.png',
+'images/dice3.png',
+'images/dice4.png',
+'images/dice5.png',
+'images/dice6.png'
+];
+
+function dicee () {
+    var randomNumber1 = Math.floor(Math.random() * imagesArray.length);
+    document.querySelector(".img1").src = imagesArray[randomNumber1];
+    
+    var randomNumber2 = Math.floor(Math.random() * imagesArray.length);
+    document.querySelector(".img2").src = imagesArray[randomNumber2];
+
+    var heading = document.querySelector('h1');
+    if (randomNumber1 > randomNumber2) {
+        heading.innerText = "Player 1 Wins";
+    } else if (randomNumber1 < randomNumber2) {
+        heading.innerText = "Player 2 Wins";
+    } else if (randomNumber1 === randomNumber2) {
+        heading.innerText = "Draw";
+    }
+};        
+
+dicee();
+
+
+
+
+ 
+ 
